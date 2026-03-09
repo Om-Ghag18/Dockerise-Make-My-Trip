d5a824871f54cc5abad63a8728cf67510ae6f005
Om-Ghag <omghag18.og@gmail.com>
2026-03-09 16:01:11 +0530

    Create App.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./pages/home";
+import Bookings from "./pages/booking";
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <hr />
+      <Bookings />
+    </div>
+  );
+}
+
+export default App;
